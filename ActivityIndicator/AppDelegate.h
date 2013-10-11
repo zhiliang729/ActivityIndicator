986dd861b39e406e17ff4c3dc72e7ae753291a68
zhiliang <zhiliang729@163.com>
2013-10-11 10:49:16 +0800

    Initial Commit

---
+++ ActivityIndicator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ActivityIndicator
+//
+//  Created by zhangliang on 13-10-11.
+//  Copyright (c) 2013年 com.gooagoo.Gooagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
